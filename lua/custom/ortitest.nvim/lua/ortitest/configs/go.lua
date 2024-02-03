6c3fd604ef2a78ba418c31e8b2a5037793832e06
polivera <punk.moon2229@fastmail.com>
2024-02-03 17:53:41 +0100

    Working on ortitest
    - Switching machine

--- lua/custom/ortitest.nvim/lua/ortitest/configs/go.lua
+++ lua/custom/ortitest.nvim/lua/ortitest/configs/go.lua
@@ -1,12 +1,25 @@
+local util = require("ortitest.util")
+
+local defaultSourceFile = util.getcurrentfilename()
+local defaultSourcePath = util.getcurrentfilepath()
+local defaultTestPath = defaultSourcePath
+
 local GoConf = {
 	test = "some test",
 }
 
-GoConf.new = function()
+GoConf.new = function(sourcePath, sourceFile, testPath)
 	local self = setmetatable({}, GoConf)
-	self.test = "something"
+	self.sourcePath = sourcePath or defaultSourcePath
+	self.sourceFile = sourceFile or defaultSourceFile
+	self.testPath = testPath or defaultTestPath
+
+	-- find test file
 
 	return self
 end
+
+local foo = GoConf.new()
+print(foo)
 
 return GoConf
--- lua/custom/ortitest.nvim/lua/ortitest/util.lua
+++ lua/custom/ortitest.nvim/lua/ortitest/util.lua
@@ -5,3 +5,9 @@ local CURRENT_BUFF_ID = 0
 U.getcurrentfilename = function()
 	return vim.api.nvim_buf_get_name(CURRENT_BUFF_ID):match("^.+/(.+)$")
 end
+
+U.getcurrentfilepath = function()
+	return "something"
+end
+
+return U
